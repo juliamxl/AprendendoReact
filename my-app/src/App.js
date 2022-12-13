9ac4a39564364f36bd1d93ab34641429e64aaebd
juyyaa <juliaamarxal@gmail.com>
2022-12-13 11:36:52 -0300

    times

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -54,7 +54,7 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time nome= {time.nome}/>)}
+      {times.map(time => <Time key={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} nome= {time.nome}/>)}
     </div>
   );
 }
